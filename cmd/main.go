2b6998716407ee857345118234f43ac766c3370f
alok <aloknerurkar@no-reply.com>
2022-06-16 20:52:49 +0530

    adding commands

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 
+	fs "github.com/aloknerurkar/bee-afs/pkg/fuse"
+	"github.com/aloknerurkar/bee-afs/pkg/lookuper"
 	"github.com/aloknerurkar/bee-afs/pkg/mounts"
+	"github.com/aloknerurkar/bee-afs/pkg/publisher"
 	"github.com/aloknerurkar/bee-afs/pkg/store/beestore"
+	"github.com/billziss-gh/cgofuse/fuse"
 	"github.com/ethersphere/bee/pkg/crypto"
 	filekeystore "github.com/ethersphere/bee/pkg/keystore/file"
 	"github.com/urfave/cli/v2"
@@ -22,20 +29,19 @@ func main() {
 			Usage:   "Load configuration from `FILE`",
 			EnvVars: []string{"BEEAFS_CONFIG"},
 		},
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "swarm-key", Usage: "path to swarm-key file", Required: true}),
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "password", Usage: "password for swarm-key file", Required: true}),
+		&cli.BoolFlag{Name: "inmem", Usage: "use inmem storage for testing"},
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "swarm-key", Usage: "path to swarm-key file"}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "password", Usage: "password for swarm-key file"}),
 		altsrc.NewStringFlag(&cli.StringFlag{Name: "api-host", DefaultText: "http://localhost"}),
 		altsrc.NewIntFlag(&cli.IntFlag{Name: "api-port", DefaultText: "1633"}),
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "postage-batch", Required: true}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "postage-batch"}),
 		altsrc.NewBoolFlag(&cli.BoolFlag{Name: "pin"}),
 		altsrc.NewBoolFlag(&cli.BoolFlag{Name: "encrypt"}),
 	}
 
 	app := &cli.App{
-		Name:   "bee-afs",
-		Usage:  "Provides filesystem abstraction for Swarm decentralized storage",
-		Before: altsrc.InitInputSourceWithContext(confFlags, altsrc.NewYamlSourceFromFlagFunc("config")),
-		Flags:  confFlags,
+		Name:  "bee-afs",
+		Usage: "Provides filesystem abstraction for Swarm decentralized storage",
 		Commands: []*cli.Command{
 			{
 				Name:    "mount",
@@ -43,9 +49,21 @@ func main() {
 				Usage:   "Mount a FUSE Filesystem on Swarm",
 				Subcommands: []*cli.Command{
 					{
-						Name: "list",
+						Name:   "list",
+						Before: altsrc.InitInputSourceWithContext(confFlags, altsrc.NewYamlSourceFromFlagFunc("config")),
+						Flags:  confFlags,
 						Action: func(c *cli.Context) error {
-							var mnts mounts.UserMounts
+							st, err := getBeeStore(c)
+							if err != nil {
+								return err
+							}
+
+							lk, pb, err := getLookuperPublisher(c, st)
+							if err != nil {
+								return err
+							}
+
+							mnts := mounts.New(lk, pb, st)
 							mntList, err := mnts.Get(c.Context)
 							if err != nil {
 								return fmt.Errorf("failed getting mounts for user %w", err)
@@ -64,13 +82,25 @@ func main() {
 						},
 					},
 					{
-						Name: "create",
+						Name:   "create",
+						Before: altsrc.InitInputSourceWithContext(confFlags, altsrc.NewYamlSourceFromFlagFunc("config")),
+						Flags:  confFlags,
 						Action: func(c *cli.Context) error {
 							if c.NArg() != 2 {
 								return errors.New("incorrect arguments")
 							}
 
-							var mnts mounts.UserMounts
+							st, err := getBeeStore(c)
+							if err != nil {
+								return err
+							}
+
+							lk, pb, err := getLookuperPublisher(c, st)
+							if err != nil {
+								return err
+							}
+
+							mnts := mounts.New(lk, pb, st)
 							mntList, err := mnts.Get(c.Context)
 							if err != nil {
 								return fmt.Errorf("failed getting mounts for user %w", err)
@@ -89,6 +119,39 @@ func main() {
 								}
 							}
 
+							opts := []fs.Option{fs.WithNamespace(c.Args().Get(0))}
+							if c.Bool("encrypt") {
+								opts = append(opts, fs.WithEncryption(true))
+							}
+
+							fsImpl, err := fs.New(st, lk, pb, opts...)
+							if err != nil {
+								return fmt.Errorf("failed creating new fs %w", err)
+							}
+
+							interruptChannel := make(chan os.Signal, 1)
+							signal.Notify(interruptChannel, syscall.SIGINT, syscall.SIGTERM)
+
+							srv := fuse.NewFileSystemHost(fsImpl)
+							srv.SetCapReaddirPlus(true)
+							var fuseArgs []string
+							if c.Bool("debug") {
+								fuseArgs = []string{"-d"}
+							}
+							stopped := make(chan struct{})
+							go func() {
+								if !srv.Mount(c.Args().Get(1), fuseArgs) {
+									close(stopped)
+								}
+							}()
+
+							select {
+							case <-stopped:
+								return errors.New("fuse mount stopped")
+							case <-interruptChannel:
+								fmt.Println("Received stop signal...")
+							}
+
 							return nil
 						},
 					},
@@ -96,15 +159,32 @@ func main() {
 			},
 		},
 	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Printf("failed err: %s\n", err.Error())
+	}
 }
 
 func getSigner(c *cli.Context) (crypto.Signer, error) {
 	keystore := filekeystore.New(filepath.Dir(c.String("swarm-key")))
 	pk, _, err := keystore.Key("swarm", c.String("password"))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed reading swarm key %w", err)
 	}
 	return crypto.NewDefaultSigner(pk), nil
+}
+
+func getLookuperPublisher(c *cli.Context, b *beestore.BeeStore) (lookuper.Lookuper, publisher.Publisher, error) {
+	signer, err := getSigner(c)
+	if err != nil {
+		return nil, nil, err
+	}
+	owner, err := signer.EthereumAddress()
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed getting owner eth address %w", err)
+	}
+	return lookuper.New(b, owner), publisher.New(b, signer), nil
 }
 
 func getBeeStore(c *cli.Context) (*beestore.BeeStore, error) {
--- pkg/mounts/mounts.go
+++ pkg/mounts/mounts.go
@@ -1,8 +1,95 @@
 package mounts
 
-import "context"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/aloknerurkar/bee-afs/pkg/lookuper"
+	"github.com/aloknerurkar/bee-afs/pkg/publisher"
+	"github.com/aloknerurkar/bee-afs/pkg/store"
+	"github.com/ethersphere/bee/pkg/file/joiner"
+	"github.com/ethersphere/bee/pkg/file/splitter"
+	"github.com/ethersphere/bee/pkg/storage"
+	"github.com/ethersphere/bee/pkg/swarm"
+)
+
+const userMounts string = "userMounts"
 
 type UserMounts interface {
 	Get(ctx context.Context) ([]string, error)
 	Put(ctx context.Context, mnts []string) error
+}
+
+func isAllZeroes(addr swarm.Address) bool {
+	return swarm.NewAddress(make([]byte, 32)).Equal(addr)
+}
+
+type userMountsImpl struct {
+	lk lookuper.Lookuper
+	pb publisher.Publisher
+	st store.PutGetter
+}
+
+func New(lk lookuper.Lookuper, pb publisher.Publisher, st store.PutGetter) UserMounts {
+	return &userMountsImpl{lk, pb, st}
+}
+
+func (u *userMountsImpl) Get(ctx context.Context) ([]string, error) {
+	ref, err := u.lk.Get(ctx, userMounts, time.Now().Unix())
+	if err != nil || isAllZeroes(ref) {
+		// return empty for now
+		return []string{}, nil
+	}
+	reader, _, err := joiner.New(ctx, u.st, ref)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating reader %w", err)
+	}
+	buf, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read %w", err)
+	}
+	mntList := []string{}
+	err = json.Unmarshal(buf, &mntList)
+	if err != nil {
+		return nil, fmt.Errorf("failed unmarshaling data %w", err)
+	}
+	return mntList, nil
+}
+
+type mountsReader struct {
+	*bytes.Buffer
+}
+
+func (m *mountsReader) Close() error {
+	m.Reset()
+	return nil
+}
+
+func (u *userMountsImpl) Put(ctx context.Context, mnts []string) error {
+	if len(mnts) == 0 {
+		return errors.New("mount list empty")
+	}
+
+	buf, err := json.Marshal(mnts)
+	if err != nil {
+		return fmt.Errorf("failed marshaling data %w", err)
+	}
+
+	sp := splitter.NewSimpleSplitter(u.st, storage.ModePutUpload)
+	addr, err := sp.Split(ctx, &mountsReader{bytes.NewBuffer(buf)}, int64(len(buf)), false)
+	if err != nil {
+		return fmt.Errorf("failed splitter %w", err)
+	}
+
+	err = u.pb.Put(ctx, userMounts, time.Now().Unix(), addr)
+	if err != nil {
+		return fmt.Errorf("failed publishing %w", err)
+	}
+
+	return nil
 }
