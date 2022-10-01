1d3fe5e15308065fa43fdd78e60730adad2f8834
alok <aloknerurkar@no-reply.com>
2022-10-01 17:03:33 +0530

    feat: add state to publisher

--- pkg/publisher/publisher.go
+++ pkg/publisher/publisher.go
@@ -2,12 +2,13 @@ package publisher
 
 import (
 	"context"
+	"encoding/binary"
 	"fmt"
+	"strconv"
 	"sync"
 
 	"github.com/ethersphere/bee/pkg/crypto"
 	"github.com/ethersphere/bee/pkg/feeds"
-	"github.com/ethersphere/bee/pkg/feeds/sequence"
 	"github.com/ethersphere/bee/pkg/storage"
 	"github.com/ethersphere/bee/pkg/swarm"
 	logger "github.com/ipfs/go-log/v2"
@@ -19,36 +20,90 @@ type Publisher interface {
 	Put(ctx context.Context, id string, version int64, ref swarm.Address) error
 }
 
+type Loader func(ctx context.Context, id string) (feeds.Index, int64, error)
+
 type pubImpl struct {
 	putter     storage.Putter
 	signer     crypto.Signer
+	loader     Loader
 	updaterMap sync.Map
 }
 
-func New(putter storage.Putter, signer crypto.Signer) Publisher {
-	return &pubImpl{putter: putter, signer: signer}
+type feedState struct {
+	currIndex feeds.Index
+	ts        int64
+}
+
+func New(putter storage.Putter, signer crypto.Signer, loader Loader) Publisher {
+	return &pubImpl{putter: putter, signer: signer, loader: loader}
 }
 
 func (p *pubImpl) Put(ctx context.Context, id string, version int64, ref swarm.Address) error {
-	upd, err := p.updater(id)
+	var nxtIndex feeds.Index
+
+	state, found := p.updaterMap.Load(id)
+	if !found {
+		currIndex, at, err := p.loader(ctx, id)
+		if err == nil {
+			nxtIndex = currIndex.Next(at, uint64(version))
+		} else {
+			nxtIndex = new(index)
+		}
+	} else {
+		fstate := state.(feedState)
+		nxtIndex = fstate.currIndex.Next(fstate.ts, uint64(version))
+	}
+
+	err := p.update(ctx, id, version, nxtIndex, ref.Bytes())
+	if err != nil {
+		return fmt.Errorf("publisher: failed to update next index: %w", err)
+	}
+
+	p.updaterMap.Store(id, feedState{currIndex: nxtIndex, ts: version})
+
+	log.Debugf("updated id %s version %d ref %s", id, version, ref.String())
+
+	return nil
+}
+
+func (p *pubImpl) update(
+	ctx context.Context,
+	id string,
+	version int64,
+	idx feeds.Index,
+	payload []byte,
+) error {
+	putter, err := feeds.NewPutter(p.putter, p.signer, []byte(id))
 	if err != nil {
 		return err
 	}
 
-	log.Debugf("updating id %s version %d ref %s", id, version, ref.String())
+	err = putter.Put(ctx, idx, version, payload)
+	if err != nil {
+		return err
+	}
 
-	return upd.Update(ctx, version, ref.Bytes())
+	return nil
 }
 
-func (p *pubImpl) updater(id string) (feeds.Updater, error) {
-	upd, found := p.updaterMap.Load(id)
-	if !found {
-		var err error
-		upd, err = sequence.NewUpdater(p.putter, p.signer, []byte(id))
-		if err != nil {
-			return nil, fmt.Errorf("failed creating new updater %w", err)
-		}
-		p.updaterMap.Store(id, upd)
-	}
-	return upd.(feeds.Updater), nil
+// index replicates the feeds.sequence.Index. This index creation is not exported from
+// the package as a result loader doesn't know how to return the first feeds.Index.
+// This index will be used to return which will provide a compatible interface to
+// feeds.sequence.Index.
+type index struct {
+	index uint64
+}
+
+func (i *index) String() string {
+	return strconv.FormatUint(i.index, 10)
+}
+
+func (i *index) MarshalBinary() ([]byte, error) {
+	indexBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(indexBytes, i.index)
+	return indexBytes, nil
+}
+
+func (i *index) Next(last int64, at uint64) feeds.Index {
+	return &index{i.index + 1}
 }
--- pkg/publisher/publisher_test.go
+++ pkg/publisher/publisher_test.go
@@ -3,6 +3,7 @@ package publisher_test
 import (
 	"context"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"testing"
 	"time"
@@ -14,11 +15,11 @@ import (
 	"github.com/ethersphere/bee/pkg/feeds/sequence"
 	"github.com/ethersphere/bee/pkg/storage/mock"
 	"github.com/ethersphere/bee/pkg/swarm"
-	logger "github.com/ipfs/go-log/v2"
 )
 
 func TestPublisher(t *testing.T) {
-	logger.SetLogLevel("*", "Error")
+	t.Parallel()
+
 	st := mock.NewStorer()
 
 	pk, _ := crypto.GenerateSecp256k1Key()
@@ -29,33 +30,101 @@ func TestPublisher(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	pub := publisher.New(st, signer)
+	t.Run("new", func(t *testing.T) {
+		pub := publisher.New(
+			st,
+			signer,
+			func(_ context.Context, _ string) (feeds.Index, int64, error) {
+				return nil, 0, errors.New("some error")
+			},
+		)
 
-	for pfx, id := range []string{"test1", "test2", "test3"} {
-		lk := sequence.NewFinder(st, feeds.New([]byte(id), owner))
-		var hint int64
-		for idx := 0; idx < 3; idx++ {
-			t.Run(fmt.Sprintf("topic=%s/idx=%d", id, idx), func(t *testing.T) {
-				ref := swarm.MustParseHexAddress(fmt.Sprintf("%d%d00000000000000000000000000000000000000000000000000000000000000", pfx, idx))
-				err := pub.Put(context.TODO(), id, time.Now().Unix(), ref)
-				if err != nil {
-					t.Fatal(err)
-				}
-				time.Sleep(time.Second)
-				ch, current, _, err := lk.At(context.TODO(), time.Now().Unix(), hint)
-				if err != nil {
-					t.Fatal(err)
-				}
-				ref2, _, err := lookuper.ParseFeedUpdate(ch)
-				if err != nil {
-					t.Fatal(err)
-				}
-				if !ref2.Equal(ref) {
-					t.Fatalf("incorrect ref in lookup exp %s found %s", ref.String(), ref2.String())
-				}
-				buf, _ := current.MarshalBinary()
-				hint = int64(binary.BigEndian.Uint64(buf))
-			})
+		for pfx, id := range []string{"test1", "test2", "test3"} {
+			lk := sequence.NewFinder(st, feeds.New([]byte(id), owner))
+			var hint int64
+			for idx := 0; idx < 3; idx++ {
+				t.Run(fmt.Sprintf("topic=%s/idx=%d", id, idx), func(t *testing.T) {
+					ref := swarm.MustParseHexAddress(
+						fmt.Sprintf(
+							"%d%d00000000000000000000000000000000000000000000000000000000000000",
+							pfx, idx,
+						),
+					)
+					err := pub.Put(context.TODO(), id, time.Now().Unix(), ref)
+					if err != nil {
+						t.Fatal(err)
+					}
+					time.Sleep(time.Second)
+					ch, current, _, err := lk.At(context.TODO(), time.Now().Unix(), hint)
+					if err != nil {
+						t.Fatal(err)
+					}
+					ref2, _, err := lookuper.ParseFeedUpdate(ch)
+					if err != nil {
+						t.Fatal(err)
+					}
+					if !ref2.Equal(ref) {
+						t.Fatalf("incorrect ref in lookup exp %s found %s", ref.String(), ref2.String())
+					}
+					buf, _ := current.MarshalBinary()
+					hint = int64(binary.BigEndian.Uint64(buf))
+				})
+			}
 		}
-	}
+	})
+
+	t.Run("existing state", func(t *testing.T) {
+		pub := publisher.New(
+			st,
+			signer,
+			func(ctx context.Context, id string) (feeds.Index, int64, error) {
+				lk := sequence.NewFinder(st, feeds.New([]byte(id), owner))
+				ch, start, _, err := lk.At(ctx, time.Now().Unix(), 0)
+				if err != nil {
+					return nil, 0, err
+				}
+				if ch == nil {
+					return nil, 0, errors.New("invalid chunk")
+				}
+				_, ts, err := lookuper.ParseFeedUpdate(ch)
+				if err != nil {
+					return nil, 0, err
+				}
+				return start, ts, nil
+			},
+		)
+
+		for pfx, id := range []string{"test1", "test2", "test3"} {
+			lk := sequence.NewFinder(st, feeds.New([]byte(id), owner))
+			var hint int64
+			for idx := 3; idx < 6; idx++ {
+				t.Run(fmt.Sprintf("topic=%s/idx=%d", id, idx), func(t *testing.T) {
+					ref := swarm.MustParseHexAddress(
+						fmt.Sprintf(
+							"%d%d00000000000000000000000000000000000000000000000000000000000000",
+							pfx, idx,
+						),
+					)
+					err := pub.Put(context.TODO(), id, time.Now().Unix(), ref)
+					if err != nil {
+						t.Fatal(err)
+					}
+					time.Sleep(time.Second)
+					ch, current, _, err := lk.At(context.TODO(), time.Now().Unix(), hint)
+					if err != nil {
+						t.Fatal(err)
+					}
+					ref2, _, err := lookuper.ParseFeedUpdate(ch)
+					if err != nil {
+						t.Fatal(err)
+					}
+					if !ref2.Equal(ref) {
+						t.Fatalf("incorrect ref in lookup exp %s found %s", ref.String(), ref2.String())
+					}
+					buf, _ := current.MarshalBinary()
+					hint = int64(binary.BigEndian.Uint64(buf))
+				})
+			}
+		}
+	})
 }
